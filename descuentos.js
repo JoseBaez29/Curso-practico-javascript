65ca27adb05aa1209b106554378002496b299f4e
Jose Baez <josebaez2995@gmail.com>
2021-11-04 23:18:16 -0400

    integrando HTML, para pedir datos del usuario y manipulando etiquetas de HTML con JavaScrpt

--- descuentos.js
+++ descuentos.js
@@ -3,3 +3,14 @@ function porcentajesYdescuentos(precio, descuento) {
   const precioConDescuento = (precio * DescuentoAplicarPrecio) / 100;
   return precioConDescuento;
 }
+
+function btn_PriceDiscount() {
+  const inputPrice = document.getElementById("input_Price");
+  const priceValue = Number(inputPrice.value);
+  const inputDiscount = document.getElementById("input_Discount");
+  const DiscountValue = Number(inputDiscount.value);
+
+  const priceFinal = porcentajesYdescuentos(priceValue, DiscountValue);
+  const resultPrice = document.getElementById("resultPrice");
+  resultPrice.innerText = "El precio a pagar el $" + priceFinal;
+}
