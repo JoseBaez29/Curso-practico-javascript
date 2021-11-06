5020c9c475ec8a9990b04440d7d830b6b9f77f48
Jose Baez <josebaez2995@gmail.com>
2021-11-06 19:34:51 -0400

    Applying the discount by means of coupons

--- descuentos.js
+++ descuentos.js
@@ -4,13 +4,33 @@ function porcentajesYdescuentos(precio, descuento) {
   return precioConDescuento;
 }
 
+const cupon = ["regular", "confiable", "preferencial"];
+
+console.log(cupon);
 function btn_PriceDiscount() {
   const inputPrice = document.getElementById("input_Price");
   const priceValue = Number(inputPrice.value);
-  const inputDiscount = document.getElementById("input_Discount");
-  const DiscountValue = Number(inputDiscount.value);
+  const inputcuponDiscount = document.getElementById("input_CuponDiscount");
+  const cuponDiscountValue = inputcuponDiscount.value;
 
-  const priceFinal = porcentajesYdescuentos(priceValue, DiscountValue);
+  let descuento = 0;
+  if (!cupon.includes(cuponDiscountValue)) {
+    alert("El cupón " + cuponDiscountValue + " no existe!!");
+  } else {
+    switch (cuponDiscountValue) {
+      case cupon[0]:
+        descuento = 10;
+        break;
+      case cupon[1]:
+        descuento = 30;
+        break;
+      case cupon[2]:
+        descuento = 50;
+        break;
+    }
+  }
+
+  const priceFinal = porcentajesYdescuentos(priceValue, descuento);
   const resultPrice = document.getElementById("resultPrice");
   resultPrice.innerText = "El precio a pagar el $" + priceFinal;
 }
